fb395760ff67ae7c5da80e38eb45230befa5a424
Ryan Quiogue <ryan.quiogue@adp.com>
2023-07-05 14:06:55 -0400

    Front End Basics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>a</div>
+    <>
+      <Header></Header>
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer className=''></Footer>
+    </>
   )
 }
 
