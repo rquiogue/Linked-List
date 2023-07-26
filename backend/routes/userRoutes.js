4f82e224d75e8a510322d794403b875469e5423f
Ryan Quiogue <ryan.quiogue@adp.com>
2023-07-26 16:47:00 -0400

    Finished back end Authentication

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+const router = express.Router();
+import {
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile,
+    getUsers,
+    getUserByID,
+    deleteUser,
+    updateUser,
+} from '../controllers/userController.js'
+
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').post(registerUser).get(getUsers);
+router.post('/logout', logoutUser);
+router.post('/login', authUser);
+router.route('/profile').get(protect, getUserProfile).put( protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserByID).put(protect, admin, updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import products from './data/products.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js'
 dotenv.config();
 
 connectDB();
@@ -11,11 +13,19 @@ connectDB();
 const port = process.env.PORT || 5000;
 const app = express();
 
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
